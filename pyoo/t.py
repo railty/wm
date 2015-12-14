ce92974d3642128c4938904129ea6f9541f52bdb
shawn ning <zxning@gmail.com>
2015-12-13 23:04:02 -0500

    update

--- pyoo/t.py
+++ pyoo/t.py
@@ -22,5 +22,28 @@ doc = desktop.open_spreadsheet("Week 47 Pricing Guide.xlsx")
 print(doc.sheets)
 
 pdb.set_trace()
+
+#sheet = doc.sheets[0]
+#sheet = doc.sheets['Al Premium Specific Items']
+#if doc.sheets[0][3,11].value == 'WM#'
+#p doc.sheets[0][5,11].value
+#type(doc.sheets[0][5,11].value) = float
+#int(doc.sheets[0][5,11].value)
+#p type(doc.sheets[0][344,11].value)
+
+# p isinstance(doc.sheets[0][343,11].value, float)
+#True
+#(Pdb) p isinstance(doc.sheets[0][344,11].value, float)
+#False
+#(Pdb) p isinstance(doc.sheets[0][344,11].value, str)
+#p doc.sheets[0][340,14].background_color
+#16776960
+#doc.sheets.copy('My Sheet', 'Copied Sheet', 2)
+#doc.sheets.copy('Al Premium Specific Items', 'ALP Worksheet', 4)
+#len(doc.sheets)
+#doc.save('example.xlsx', pyoo.FILTER_EXCEL_2007)
+
+
+
 doc.close()
 subprocess.call("kill `ps|grep soffice.bin| awk '{print $1}'`", shell=True)
